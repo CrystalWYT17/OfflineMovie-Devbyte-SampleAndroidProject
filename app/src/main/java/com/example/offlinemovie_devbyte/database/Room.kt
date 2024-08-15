c16be1d6d3b15b501ce585ec2907dc208ad22e63
CrystalWYT17 <wuttyeetun1721999@gmail.com>
2024-08-15 11:22:35 +0800

    create db, get db

--- app/src/main/java/com/example/offlinemovie_devbyte/database/Room.kt
+++ app/src/main/java/com/example/offlinemovie_devbyte/database/Room.kt
@@ -17,15 +17,43 @@
 
 package com.example.offlinemovie_devbyte.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
+import androidx.room.Database
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
-interface VideoDao{
+interface VideoDao {
     @Query("Select * from databasevideo")
     fun getVideos(): LiveData<List<DatabaseVideo>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAll(vararg videos:DatabaseVideo)
+    fun insertAll(vararg videos: DatabaseVideo)
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase : RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+//        not initialized
+            INSTANCE =
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    VideosDatabase::class.java,
+                    "videos"
+                )
+                    .build()
+        }
+    }
+
+    return INSTANCE
 }
